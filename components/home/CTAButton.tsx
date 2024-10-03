5c50ec7f8f64d127d775ce3c8f5067f8f26ae9e7
shelowpanda <190083386@qq.com>
2024-10-03 21:09:02 +0800

    修改模板内容

--- components/home/CTAButton.tsx
+++ components/home/CTAButton.tsx
@@ -1,21 +1,20 @@
 import { Button } from "@/components/ui/button";
-import { RocketIcon } from "lucide-react";
 import Link from "next/link";
 
 const CTAButton = ({ locale }: { locale: any }) => {
   return (
     <Link
-      href="https://github.com/weijunext/landing-page-boilerplate"
-      target="_blank"
-      rel="noopener noreferrer nofollow"
+    // href="https://github.com/weijunext/landing-page-boilerplate"
+    // target="_blank"
+    // rel="noopener noreferrer nofollow"
     >
       <Button
-        variant="default"
-        className="flex items-center gap-2 bg-blue-500 hover:bg-blue-600 text-white"
-        aria-label="Get Boilerplate"
+      // variant="default"
+      // className="flex items-center gap-2 bg-blue-500 hover:bg-blue-600 text-white"
+      // aria-label="Get Boilerplate"
       >
-        <RocketIcon />
-        {locale.title}
+        {/* <RocketIcon /> */}
+        {/* {locale.title} */}
       </Button>
     </Link>
   );

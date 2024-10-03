258ee5ffda92303114f47b4158368c06ee1d065f
shelowpanda <190083386@qq.com>
2024-10-03 21:25:02 +0800

    修改模板

--- components/home/CTAButton.tsx
+++ components/home/CTAButton.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 const CTAButton = ({ locale }: { locale: any }) => {
   return (
     <Link
-    // href="https://github.com/weijunext/landing-page-boilerplate"
-    // target="_blank"
-    // rel="noopener noreferrer nofollow"
+      href="https://github.com/weijunext/landing-page-boilerplate"
+      target="_blank"
+      rel="noopener noreferrer nofollow"
     >
       <Button
       // variant="default"
